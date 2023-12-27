e3559e6be57a8b4fd87fc902e5b4af7879bb4842
Denis Sorrilha <denis.sorrilha@gmail.com>
2023-12-27 14:43:40 -0300

    Explorando C#!

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,24 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa("Denis", "Sorrilha");
+
+
+Pessoa p2 = new Pessoa("Douglas", "Pereira");
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+// Pessoa p1 = new Pessoa();
+
+// p1.Nome = "Denis";
+// p1.Sobrenome = "Sorrilha";
+// p1.Idade = 42;
+// p1.Apresentar();
