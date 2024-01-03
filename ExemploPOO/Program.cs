daa99d5ee30a4f386e926ff6008f1202dcbd009e
Denis Sorrilha <denis.sorrilha@gmail.com>
2024-01-03 13:43:41 -0300

    Classes abstratas em C#

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -1,19 +1,24 @@
 ﻿using ExemploPOO.Models;
 
 
-Aluno a1 = new Aluno();
-a1.Nome = "Denis";
-a1.Idade = 42;
-a1.Email = "denis@sorrilha.com";
-a1.Nota = 10;
-a1.Apresentar();
+Corrente c = new Corrente();
+c.Creditar(500);
+c.ExibirSaldo();
 
 
-Professor p1 = new Professor();
-p1.Nome = "Roberto";
-p1.Idade = 56;
-p1.Salario = 2000;
-p1.Apresentar();
+// Aluno a1 = new Aluno();
+// a1.Nome = "Denis";
+// a1.Idade = 42;
+// a1.Email = "denis@sorrilha.com";
+// a1.Nota = 10;
+// a1.Apresentar();
+
+
+// Professor p1 = new Professor();
+// p1.Nome = "Roberto";
+// p1.Idade = 56;
+// p1.Salario = 2000;
+// p1.Apresentar();
 
 
 
