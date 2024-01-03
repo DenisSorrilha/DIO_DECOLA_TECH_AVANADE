4f2f3a582e690db3cd6ceb41e76fc381eb111389
Denis Sorrilha <denis.sorrilha@gmail.com>
2024-01-03 13:23:35 -0300

    POO em C#

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,41 @@
+﻿using ExemploPOO.Models;
+
+
+Aluno a1 = new Aluno();
+a1.Nome = "Denis";
+a1.Idade = 42;
+a1.Email = "denis@sorrilha.com";
+a1.Nota = 10;
+a1.Apresentar();
+
+
+Professor p1 = new Professor();
+p1.Nome = "Roberto";
+p1.Idade = 56;
+p1.Salario = 2000;
+p1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(100);
+// c1.ExibirSaldo();
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Denis";
+// p1.Idade = 42;
+
+// p1.Apresentar();
