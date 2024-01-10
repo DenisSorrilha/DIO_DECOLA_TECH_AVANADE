87ff7d414b4d2a6b4344c96983c25a1ee5493e52
Denis Sorrilha <denis.sorrilha@gmail.com>
2024-01-09 21:16:38 -0300

    Testes unitários em C# finalizado.

--- ModuloTestesDIO/CalculadoraTestes/obj/Debug/net8.0/CalculadoraTestes.AssemblyInfo.cs
+++ ModuloTestesDIO/CalculadoraTestes/obj/Debug/net8.0/CalculadoraTestes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraTestes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f9b92e496e252cee28b89f853865a1aa1ac309")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraTestes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraTestes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
